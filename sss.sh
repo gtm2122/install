7401c1cd9b2b057a634d225ab6275c8e432e50a6
gtm2122 <gabrielthomas281@gmail.com>
2017-07-12 11:25:43 -0400

    new instructions and updated install

--- sss.sh
+++ sss.sh
@@ -1,3 +1,9 @@
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+brew install python
+curl -O http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
 pip install http://download.pytorch.org/whl/torch-0.1.12.post2-cp27-none-macosx_10_7_x86_64.whl 
 pip install torchvision 
 wget http://vis-www.cs.umass.edu/lfw/lfw.tgz
